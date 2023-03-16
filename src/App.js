20a3fc4889c3906f9c08ef0cb579742402506b60
Harshith <harshithdhara291@gmail.com>
2023-03-16 12:53:17 +0530

    Home page first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import HomePage from './Prabisha';
-import './App.css';
-function App() {
-  return (
-    <HomePage/>
-  );
-}
+import "./App.css";
+import TopSection from "./TopSection";
+import DigitalMarketingAgency from "./DigitalMarketingAgency";
+import UGCompany from "./UGCompany";
+import DigitalMarketingAgencyLondon from "./DigitalMarketingAgencyLondon";
+import HighQualityDigitalGrowthSolutions from "./HighQualityDigitalGrowthSolutions";
+import CountSection from "./CountSection";
+import ServicesCards from "./ServicesCards";
+import BottomSection from "./BottomSection";
+
+const App = () => (
+  <>
+    <TopSection />
+    <DigitalMarketingAgency />
+    <UGCompany />
+    <DigitalMarketingAgencyLondon />
+    <HighQualityDigitalGrowthSolutions />
+    <CountSection />
+    <ServicesCards />
+    <BottomSection />
+  </>
+);
 
 export default App;
